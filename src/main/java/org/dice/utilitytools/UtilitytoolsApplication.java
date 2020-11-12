630fa5ad4fd86da01fb5949e6b83fd4144c0737a
farshad68 <farshad.afshari68@gmail.com>
2020-11-12 13:23:07 +0100

    init

---
+++ src/main/java/org/dice/utilitytools/UtilitytoolsApplication.java
@@ -0,0 +1,13 @@
+package org.dice.utilitytools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtilitytoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtilitytoolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dice/utilitytools/UtilitytoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dice.utilitytools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtilitytoolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
